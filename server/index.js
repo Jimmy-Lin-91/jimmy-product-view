47590eda85061597d80fc6bc97f089bc649908de
Jimmy Lin <hijimmy88@gmail.com>
2021-01-13 09:42:54 -0800

    Deleted Docker.

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = require('./router.js');
 const path = require('path');
 const morgan = require('morgan');
-const port = 3002;
+const port = 3000;
 const bodyparser = require('body-parser');
 const app = express();
+const db = require('../db/index');
 
 app.use(morgan('dev'));
 //dirname is global but path goes based on relative files.
